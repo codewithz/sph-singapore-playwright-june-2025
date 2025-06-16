2dab05f859ddf577085dd3e4dbb13b9ef7089af1
ilyasdabholkar <ilyas.dabholkar@asterixsolution.com>
2025-06-16 15:55:32 +0800

    added to navigation test

--- tests/basic-actions/1-navigation.test.ts
+++ tests/basic-actions/1-navigation.test.ts
@@ -20,3 +20,29 @@ test('Back, forward, reload (refresh) test', async ({ page }) => {
     await page.reload();
     await expect(page).toHaveTitle(savingsTitle);
 });
+
+test('Navigation test', async ({ page }) => {
+
+    await page.goto('http://localhost:3000', { waitUntil: 'load', timeout: 100 });
+    await expect(page).toHaveTitle(homeTitle);
+});
+
+
+
+test.use({ navigationTimeout: 8000 });
+
+
+test('Load speed while navigating', async ({ page }) => {
+
+    await page.goto('http://localhost:3000/savings.html', { timeout: 5000 });
+    await expect(page).toHaveTitle(savingsTitle);
+
+    await page.goBack({ timeout: 8000 });
+    await expect(page).toHaveTitle(homeTitle);
+
+    await page.goForward();
+    await expect(page).toHaveTitle(savingsTitle);
+
+    await page.reload();
+    await expect(page).toHaveTitle(savingsTitle);
+});
