4a862fe2d9f99faba911acbc21a1516b040a9cce
ilyasdabholkar <ilyas.dabholkar@asterixsolution.com>
2025-06-17 14:19:03 +0800

    Config Test Level

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   workers:"50%", // Use 50% of available CPU cores for parallel execution
  
   use: {
-    headless: false, // Run tests in headful mode
+    headless: false, 
+    timezoneId: 'Asia/Singapore', // Set timezone to Singapore
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'retain-on-failure',
@@ -15,7 +16,11 @@ export default defineConfig({
       slowMo: 500, // Slow down operations by 50ms
   },
   baseURL: 'http://localhost:3000', // Base URL for tests
-}
+},
+// webServer: {
+//   command: 'npm start', // Command to start the server
+//   url: 'http://localhost:3001', // URL to wait for the server to be ready
+// }
   
 });
   
