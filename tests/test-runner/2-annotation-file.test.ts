18ecc6850454588aaf356af8003702538d77c4e6
ilyasdabholkar <ilyas.dabholkar@asterixsolution.com>
2025-06-17 11:11:05 +0800

    Group and Annotation Test

---
+++ tests/test-runner/2-annotation-file.test.ts
@@ -0,0 +1,25 @@
+import { test } from '@playwright/test';
+
+test.skip();
+
+test('Will not run 1', async () => {
+    console.log("Test 1");
+});
+
+
+test('Will not run 2', async () => {
+    console.log("Test 2");
+});
+
+
+test.fixme('Will not run 3', async () => {
+
+    console.log("Test 3");
+});
+
+
+test('Will not run 4', async () => {
+
+    test.fail();
+    console.log("Test 4");
+});
